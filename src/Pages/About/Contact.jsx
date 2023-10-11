f9f4ae6b6c2e24e51ddf5240c0d5c9d3f2af3332
Nehemiah Abah Miagen <nabah@miagen.com>
2023-10-11 11:52:31 +0100

    trial

--- src/Pages/About/Contact.jsx
+++ src/Pages/About/Contact.jsx
@@ -1,21 +1,22 @@
 import React from 'react'
 import Header from '../../components/Header'
-import Phone from '../../assets/phone.jpg'
-import styles from './Contact.module.css'
+
+
 
 const Contact = () => {
   return (
-    <div className={styles.container}>
-        <form className={styles.contactForm}>
-            <h2>Contact Us</h2>
-            <input type="text" placeholder="Your Name" />
-            <input type="email" placeholder="Your Email" />
-            <textarea placeholder="Your Message"></textarea>
-            <button type="submit">Submit</button>
+    <div className="contact-container">
+      <Header />
+      <div className="form-wrapper">
+        <form className="contactForm">
+          <h2>Contact Us</h2>
+          <input type="text" placeholder="Your Name" />
+          <input type="email" placeholder="Your Email" />
+          <textarea placeholder="Your Message"></textarea>
+          <button type="submit">Submit</button>
         </form>
+      </div>
     </div>
-);
-};
-
-
+  );
+}
 export default Contact
